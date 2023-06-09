0c672ae4821f10c79c6d77b98596a402145e28eb
xuerongNanopay <xuerong@nanopay.net>
2023-06-09 17:00:50 -0400

    refine router

--- basic/route/admin.js
+++ basic/route/admin.js
@@ -7,19 +7,13 @@ const productController = require('../controller/admin');
 const router = express.Router();
 
 //curl http://localhost:3030/admin/products/0
-router.get('/products/:productId', (req, resp, next) => {
-  productController.getProductById(req, resp, next);
-})
+router.get('/products/:productId', productController.getProductById);
 
 //curl http://localhost:3030/admin/products
-router.get('/products', (req, resp, next) => {
-  productController.getAllProducts(req, resp, next);
-})
+router.get('/products', productController.getAllProducts);
 
 //curl http://localhost:3030/admin/products -X POST --data-urlencode "title=AAA" 
-router.post('/products', (req, resp, next) => {
-  productController.addProduct(req, resp, next);
-})
+router.post('/products', productController.addProduct);
 
 // module.exports = router;
 exports.routes = router;
