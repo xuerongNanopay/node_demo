d7db1348af1aa7790f308a4c909f1fc95f49218b
xuerongNanopay <xuerong@nanopay.net>
2023-06-10 15:30:07 -0400

    add coment

--- basic/model/index.js
+++ basic/model/index.js
@@ -4,15 +4,17 @@ const User = require('./user_mysql');
 const Cart = require('./cart_mysql');
 const CartItem = require('./cart_item_mysql');
 
+// User 1:* Product
 Product.belongsTo(User, {
   constraints: true,
   onDelete: 'CASCADE'
 });
-
 User.hasMany(Product);
 
+// User 1:1 Product
 User.hasOne(Cart);
 Cart.belongsTo(User);
 
+// Cart *:* Product
 Cart.belongsToMany(Product, { through: CartItem});
 Product.belongsToMany(Cart, { through: CartItem});
