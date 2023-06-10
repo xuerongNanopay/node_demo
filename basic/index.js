8f96cfa43b7c7d9c95c31f120a3b71a81931cc63
xuerongNanopay <xuerong@nanopay.net>
2023-06-10 12:38:24 -0400

    inject user into request

--- basic/index.js
+++ basic/index.js
@@ -3,7 +3,9 @@ const path = require('path')
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const rootDir = require('./util/path')
+const User = require('./model/user_mysql')
 
 //Configure and start essential components for the application.
 require('./boot');
@@ -15,8 +17,16 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(rootDir, 'public')))
 
-app.use('/admin', adminRoute.routes);
+app.use((req, resp, next) => {
+  User
+    .findByPk(1)
+    .then(user => {
+      req.user = user;
+    })
+    .catch(err => console.log(err));
+})
 
+app.use('/admin', adminRoute.routes);
 app.use('/', erroController.pageNoFund);
 
 const server = http.createServer(app);
