fcbd2adc3cd3d4514f7c8a086ea8ad0a54a9a65f
xuerongNanopay <xuerong@nanopay.net>
2023-06-13 18:46:35 -0400

    update

--- basic/routes/admin.js
+++ basic/routes/admin.js
@@ -7,11 +7,11 @@ const router = express.Router();
 
 router.get('/add-product', isAuth, adminController.getAddProduct);
 
-router.get('/products', isAuth, adminController.getProducts);
+router.get('/products', adminController.getProducts);
 
 router.post('/add-product', isAuth, adminController.postAddProduct);
 
-router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 router.post('/edit-product', isAuth, adminController.postEditProduct);
 
