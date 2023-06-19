323a15932aebcb1c1f13d7f352c22cf7bcc188a3
xuerongNanopay <xuerong@nanopay.net>
2023-06-18 20:36:54 -0400

    add validator

--- basic_rest/graphql/resolvers.js
+++ basic_rest/graphql/resolvers.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const validator = require('validator')
 
 const User = require('../models/user');
 
@@ -12,11 +13,22 @@ module.exports = {
   signInUser: async (args, req) => {
     const { email, password } = args.userInput;
 
+    const errors = [];
+    if ( ! validator.isEmail(email) ) {
+      errors.push({message: 'Email is invalid.'})
+    }
+
     const user = await User.findOne({email})
     if ( ! user ) {
       const error = new Error('no user found');
       throw error;
-    } 
+    }
+    const isRightPassword = await bcrypt.compare(password, user.password)
+    if ( ! isRightPassword ) {
+      const error = new Error('Incorrect password');
+      throw error;
+    }
+
     return { ...user._doc, _id: user._id.toString() }
   }
 }
