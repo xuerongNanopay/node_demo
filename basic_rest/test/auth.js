1e4773ad862f764994586ed78c4eee3e6651b4e3
xuerongNanopay <xuerong@nanopay.net>
2023-06-22 19:15:17 -0400

    add test code

--- basic_rest/test/auth.js
+++ basic_rest/test/auth.js
@@ -1,5 +1,28 @@
 const {expect} = require('chai');
-const auth = require('../middleware/is-auth')
+const auth = require('../middleware/is-auth');
+const sinon = require('sinon');
+const authController = require('../controllers/auth')
+const User = require('../models/user');
+
+describe('Auth Controller', () => {
+  it('should throw an error if db fails', (done) => {
+    sinon.stub(User, 'findOne');
+    User.findOne.rejects("DB error");
+    const req = {
+      body: {
+        email: 'test@test.com',
+        passwod: 'aaa'
+      }
+    }
+    authController.login(req, {}, e => {
+      // console.log(e)
+      expect(e.statusCode).to.equal(500);
+      expect(e).to.be.an('error');
+      done();
+    })
+    User.findOne.restore();
+  })
+})
 
 it('should throw an error if no authorization header is present', () => {
   const req = {
