4c7dd4d1a3e50e42beda3baf3128e1ba36ef9cd5
xuerongNanopay <xuerong@nanopay.net>
2023-06-22 11:20:00 -0400

    add environment variable

--- basic_rest/boot.js
+++ basic_rest/boot.js
@@ -1,7 +1,7 @@
 const doMoogooseBoot = async _ => {
   const mongoose = require('mongoose');
   try {
-    await mongoose.connect('mongodb://root:123456@localhost:27017/rest_demo?authSource=admin')
+    await mongoose.connect(process.env.MONGO_URL)
   } catch ( err ) {
     console.log(err);
     throw new Error(err);
