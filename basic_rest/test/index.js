acac35f159c53b36663dc27309a568e426f70fc6
xuerongNanopay <xuerong@nanopay.net>
2023-06-22 11:32:37 -0400

    add stupid test

---
+++ basic_rest/test/index.js
@@ -0,0 +1,7 @@
+const { expect } = require('chai');
+
+it("Test Start...", () => {
+  const title = "Test Start...";
+  const inputTitle = "Test Start...";
+  expect(title).to.equal(inputTitle);
+})
