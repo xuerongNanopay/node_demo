c8e247be7d75c52bd9025dd50b1c9e64b6822872
xuerongNanopay <xuerong@nanopay.net>
2023-06-23 16:06:37 -0400

    basic node ts project set up

--- node_ts/src/routes/todo.ts
+++ node_ts/src/routes/todo.ts
@@ -5,12 +5,15 @@ const todoRouter: Router = Router();
 
 const todos: Array<Todo> = [];
 
+type RequestParams = {todoId: string};
 todoRouter.get('/', (req, res, next) => {
+  const {todoId} = req.params as RequestParams;
   res.status(200).json({todos: todos})
 })
 
 todoRouter.post('/todo', (req, res, next) => {
   const newTodo: Todo ={ id: '111', text: 'todo' };
+  res.status(200).json(newTodo);
 })
 
 export default todoRouter;
