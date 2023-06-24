4e5add68c2f0284b4fe4dc7a5db4a78cea1671d6
xuerongNanopay <xuerong@nanopay.net>
2023-06-23 23:09:41 -0400

    update tsconfig

---
+++ basic_ts/src/app.ts
@@ -0,0 +1,6 @@
+// const button = document.querySelector('button') as HTMLButtonElement;
+const button = document.querySelector('button')!;
+
+button.addEventListener('click', _ => {
+  console.log('aaaa')
+})
