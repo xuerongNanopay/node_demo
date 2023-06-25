493847d15c410fad9e128a9f19e537887aa6e9db
xuerongNanopay <xuerong@nanopay.net>
2023-06-25 15:53:53 -0400

    validator decorator

--- basic_ts/src/app.ts
+++ basic_ts/src/app.ts
@@ -99,3 +99,56 @@ account.showBankDetail();
 
 const pkAccount = { showBankDetail: account.showBankDetail }
 pkAccount.showBankDetail()
+
+interface ValidatorConfig {
+  [property: string]: {
+    [validatorableProp: string]: string[]
+  }
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+  registeredValidators[target?.contructor?.name] = {
+    [propName]: ['required']
+  }
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target?.contructor?.name] = {
+    [propName]: ['positive']
+  }
+}
+
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if ( ! objValidatorConfig ) return true;
+  let isValid = true;
+  for ( const prop in objValidatorConfig ) {
+    for ( const validator of objValidatorConfig[prop] ) {
+      switch ( validator ) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positve':
+          isValid = isValid && obj[prop] > 0;;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+console.log(registeredValidators)
